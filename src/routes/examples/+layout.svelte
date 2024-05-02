97340d6d1ccad6bfac35764d0cbeaf10948e2e11
Jesús García <ctw@ctwhome.com>
2024-05-02 12:20:07 +0200

    gis examples

---
+++ src/routes/examples/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import { page } from '$app/stores';
+</script>
+
+<div class="w-11/12 flex flex-col items-center mx-auto">
+  <h1 class="mb-4">{$page.data.title}</h1>
+  <slot />
+  <p><a href="/">Back to Examples</a></p>
+  <p><a target="_blank" href="https://github.com/ctwhome/ruisdael">Github</a></p>
+</div>
